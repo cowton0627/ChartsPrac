f05a90e9e51a5b917c53a495088eeb7baae71bb5
Charles Cheng <charles_cheng@microjet.com.tw>
2023-07-31 15:15:37 +0800

    Initial Commit

---
+++ ChartsPrac/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChartsPrac
+//
+//  Created by Chun-Li Cheng on 2023/7/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
